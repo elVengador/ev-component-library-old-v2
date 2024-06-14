c7296d0219705648f9a751df95be0dff6d30b2ad
Lachicagladiadora <200379@unsaac.edu.pe>
2024-06-13 21:52:22 -0500

    chore: review collapsible, link and youtube video, and remove unnecessary comments

--- src/lib/Collapsible.stories.tsx
+++ src/lib/Collapsible.stories.tsx
@@ -2,28 +2,17 @@ import type { Meta, StoryObj } from "@storybook/react";
 
 import { Collapsible } from "./Collapsible";
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: "Atoms/Collapsible",
   component: Collapsible,
-  parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-    // layout: 'centered',
-  },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  parameters: {},
   tags: ["autodocs"],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  // argTypes: {
-  //   variant: { control: 'color' },
-  //   color:{control:}
-  // },
 } satisfies Meta<typeof Collapsible>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Default: Story = {
+export const CollapsibleDefault: Story = {
   args: {
     title: "Title",
     children: (
@@ -34,15 +23,5 @@ export const Default: Story = {
         similique eveniet?
       </p>
     ),
-    // color: "primary",
-    // children: "Redirect",
   },
 };
-
-// export const OpenInNewTabLink: Story = {
-//   args: {
-//     color: "primary",
-//     target: "_blank",
-//     children: "Open in new tab",
-//   },
-// };
--- src/lib/YoutubeVideo.tsx
+++ src/lib/YoutubeVideo.tsx
@@ -9,10 +9,8 @@ export const YoutubeVideo = ({
   return (
     <div className="relative w-full h-auto pb-[56.25%]">
       <iframe
-        // start=700
         src={`https://www.youtube-nocookie.com/embed/${videoId}?start=${startMilliseconds}&rel=0`}
         title="YouTube video player"
-        // frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowFullScreen
         className="absolute w-full h-full "
